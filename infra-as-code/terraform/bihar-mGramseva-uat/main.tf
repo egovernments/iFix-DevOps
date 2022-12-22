8021ab58e2d8cc4916f3a00efc594443f521b187
nikesh-eGov <48427967+nikesh-eGov@users.noreply.github.com>
2022-12-22 16:20:43 +0530

    Update main.tf

--- infra-as-code/terraform/bihar-mGramseva-uat/main.tf
+++ infra-as-code/terraform/bihar-mGramseva-uat/main.tf
@@ -124,6 +124,15 @@ module "kafka" {
   
 }
 
+data "aws_security_group" "node_sg" {
+ tags = {
+    Name = "${var.cluster_name}-eks_worker_sg"
+  }
+  depends_on = [
+   module.eks
+  ]
+}
+
 module "node-group" {  
   for_each = toset(["mgramseva-uat"])
   source = "../modules/node-pool/aws"
@@ -131,13 +140,13 @@ module "node-group" {
   cluster_name        = "${var.cluster_name}"
   node_group_name     = "${each.key}-ng"
   kubernetes_version  = "${var.kubernetes_version}"
-  security_groups     =  ["${module.network.worker_nodes_sg_id}"]
+  security_groups     =  ["${module.network.worker_nodes_sg_id}", "${data.aws_security_group.node_sg.id}"]
   subnet              = "${concat(slice(module.network.private_subnets, 0, length(var.availability_zones)))}"
   node_group_max_size = 4
   node_group_desired_size = 4
+  
   depends_on = [
-    module.network,
-    module.eks
+    module.network, module.eks
   ]
 }  
 
